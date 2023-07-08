1f4ca661be12e4ead401416a0a353ee89263e77a
aleizn <119465229+aleizn@users.noreply.github.com>
2023-07-08 11:28:15 -0500

    Aleizn

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -4,12 +4,12 @@ global.dfail('admin', m, conn)
 throw false
 }
 let pesan = args.join` `
-let oi = `*𝙼𝙴𝙽𝚂𝙰𝙹𝙴:* ${pesan}`
-let teks = `Creador Wa.me/51992621601 SI QUIERES BOT COMUNICAME
+let oi = `*𝐀𝐧𝐨𝐭𝐞𝐧𝐬𝐞 𝐏𝐚𝐣𝐞𝐫𝐨𝐬:* ${pesan}`
+let teks = `•| 𝙸𝙶: @𝐨𝐥𝐝.𝐬𝐭𝐲𝐥𝐞_𝐜.𝐭
 *⺀𝙸 𝙽 𝚅 𝙾 𝙲 𝙰 𝙽 𝙳 𝙾 - 𝙶 𝚁 𝚄 𝙿 𝙾⺀*\n\n❏ ${oi}\n\n❏ *𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂:*\n`
 for (let mem of participants) {
-teks += `┣🎃 @${mem.id.split('@')[0]}\n`}
-teks += `└𝐀𝐥𝐞𝐢𝐳𝐧-𝐌𝐃`
+teks += `┣⚜️ @${mem.id.split('@')[0]}\n`}
+teks += `└𝚂𝚊𝚡𝚡.𝚑𝚞𝚋`
 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
 }
 handler.help = ['tagall <mesaje>','invocar <mesaje>']
